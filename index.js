b0de7f8c61687130a8a6b0546a0741aa8f207400
g-mainardi <giosue.mainardi@studio.unibo.it>
2025-11-29 10:31:01 +0100

    fix: interchange and log between local or remote database

--- index.js
+++ index.js
@@ -14,12 +14,12 @@ const getMongoURI = () => {
     const secretPath = process.env.MONGO_URI_FILE;
 
     // Case 1: Docker with Secrets (Atlas)
-    if (secretPath && fs.existsSync(secretPath)) {
-        console.log("Loading URI from Docker Secret...");
+    if (withAtlas && secretPath && fs.existsSync(secretPath)) {
+        console.log("Loading Atlas URI from Docker Secret...");
         return fs.readFileSync(secretPath, 'utf8').trim();
     }
 
-    // Case 2: Fallback (Local Develop without Secrets or Local DB)
+    // Case 2: Fallback (Use Local DB)
     console.log("No Secret found, use local URI by default...");
     return process.env.MONGO_URI_LOCAL; 
 };
@@ -28,7 +28,7 @@ const connectDB = async () => {
     const uri = getMongoURI();
     try {
         await mongoose.connect(uri);
-        console.log("Connected to MongoDB Atlas via Mongoose");
+        console.log("Connected to MongoDB via Mongoose");
     } catch (error) {
         console.error("Error connecting to MongoDB Atlas:", error);
     }
